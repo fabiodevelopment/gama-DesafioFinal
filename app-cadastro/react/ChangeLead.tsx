58f6a50b077af4911d000b82dfb2a8a9f6b6c79b
Juslene Lobeu <juslenelobeu@gmail.com>
2021-08-27 19:43:34 -0300

    [feature/top] currect workspace for rubens on link axios ChangeLead.tsx

--- app-cadastro/react/ChangeLead.tsx
+++ app-cadastro/react/ChangeLead.tsx
@@ -5,37 +5,37 @@ import axios from 'axios';
 const ChangeLead: StorefrontFunctionComponent = (props) => {
 
   const order = props.query.og;
-  
-  axios.get(`https://grupo12rubens--hiringcoders202112.myvtex.com/api/oms/pvt/orders/${order}-01/conversation-message`)
-  
-  .then(function (response) {
-    // handle success
-    const email = response.data[0].to[0].email;
 
-    axios.get(`https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads/${email}`)
-  
+  axios.get(`https://grupo12rubens--hiringcoders202112.myvtex.com/api/oms/pvt/orders/${order}-01/conversation-message`)
+
     .then(function (response) {
       // handle success
-      if(response.data.Item.name && !response.data.Item.dateClient){
-        let dateClient = new Date();
-        axios({
-          method: 'patch',
-          url: `https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads/${email}`,
-          data: {
-            dateClient,
+      const email = response.data[0].to[0].email;
+
+      axios.get(`https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads/${email}`)
+
+        .then(function (response) {
+          // handle success
+          if (response.data.Item.name && !response.data.Item.dateClient) {
+            let dateClient = new Date();
+            axios({
+              method: 'patch',
+              url: `https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads/${email}`,
+              data: {
+                dateClient,
+              }
+            });
           }
-        });
-      }
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
     })
     .catch(function (error) {
       // handle error
       console.log(error);
     })
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
 
   return null
 }
