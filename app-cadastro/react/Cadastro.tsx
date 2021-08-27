9e2740717a6a44813578d9dd5d55fdc2518fd7f4
Antonio B Junior <juniorbastos.pb@hotmail.com>
2021-08-26 22:08:19 -0300

    [feature/ admin] Implementation admin cadastro op

--- app-cadastro/react/Cadastro.tsx
+++ app-cadastro/react/Cadastro.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useCssHandles } from 'vtex.css-handles'
+import axios from 'axios';
 // import { Container, Button, Form } from 'react-bootstrap';
 
 interface CadastroProps {}
@@ -8,30 +9,26 @@ const CSS_HANDLES = ['cadastroLead', 'formGroup', 'labelGroup', 'inputGroup', 'b
 
 
 const Cadastro: StorefrontFunctionComponent<CadastroProps> = ({ }) => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
 	
-	const handles = useCssHandles(CSS_HANDLES)
+	const handles = useCssHandles(CSS_HANDLES);
 
-	async function addContact() {
-		const data = {
-			body: {
-				name: formState.name,
-				email: formState.email,
-				phone: formState.phone
+	async function handleSubmit() {
+		await axios({
+			method: 'post',
+        	url: 'https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads',
+			data: {
+				name,
+				email,
+				phone,
+				dateLead: new Date(),
 			}
-		};
-		
+		})
 
-		console.log(data);
-		const apiData = await API.post('formApi', '/contact', data);
-		console.log({ apiData });
 		alert("Cadastro realizado com sucesso");
-	}
-	
-	const formState = { name: '', email: '', phone: '' };
-	
-	function updateFormState(key: string, value: string) {
-		formState[key] = value;
-	}
+	};
 
 	return (
 		<>
@@ -43,17 +40,17 @@ const Cadastro: StorefrontFunctionComponent<CadastroProps> = ({ }) => {
 				<form>
 					<div className={`${handles.formGroup}`}>
 						<label className={`${handles.labelGroup}`}>Nome:</label>
-						<input className={`${handles.inputGroup}`} placeholder="Nome" onChange={e => updateFormState('name', e.target.value)} />
+						<input className={`${handles.inputGroup}`} placeholder="Nome" value={name} onChange={e => setName(e.target.value)} />
 					</div>
 					<div className={`${handles.formGroup}`}>
 						<label className={`${handles.labelGroup}`}>Email:</label>
-						<input className={`${handles.inputGroup}`} placeholder="Email" onChange={e => updateFormState('email', e.target.value)} />
+						<input className={`${handles.inputGroup}`} placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
 					</div>
 					<div className={`${handles.formGroup}`}>
 						<label className={`${handles.labelGroup}`}>Telefone:</label>
-						<input className={`${handles.inputGroup}`} placeholder="Telefone" onChange={e => updateFormState('phone', e.target.value)} />
+						<input className={`${handles.inputGroup}`} placeholder="Telefone" value={phone} onChange={e => setPhone(e.target.value)} />
 					</div>
-					<button className={`${handles.buttonGroup}`} onClick={addContact}>Enviar</button>
+					<button className={`${handles.buttonGroup}`} onClick={handleSubmit}>Enviar</button>
 				</form>
 			</div>
 			<div className={`${handles.modalOverlay}`}></div>
