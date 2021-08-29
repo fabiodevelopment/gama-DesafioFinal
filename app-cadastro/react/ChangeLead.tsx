5cccde3c5b26764c8b2d3f854a7faae693d0bc53
fabiodevelopment <fabiohpaes@gmail.com>
2021-08-29 01:00:38 -0300

    QA Ajustments Resolve Conflicts

--- app-cadastro/react/ChangeLead.tsx
+++ app-cadastro/react/ChangeLead.tsx
@@ -15,32 +15,26 @@ const ChangeLead: StorefrontFunctionComponent = (props) => {
   
     .then(function (response) {
       // handle success
-      const email = response.data[0].to[0].email;
-
-      axios.get(`https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads/${email}`)
-
-        .then(function (response) {
-          // handle success
-          if (response.data.Item.name && !response.data.Item.dateClient) {
-            let dateClient = new Date();
-            axios({
-              method: 'patch',
-              url: `https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads/${email}`,
-              data: {
-                dateClient,
-              }
-            });
+      if(response.data.Item.name && !response.data.Item.dateClient){
+        let dateClient = new Date();
+        axios({
+          method: 'patch',
+          url: `https://azzk045g2g.execute-api.us-east-2.amazonaws.com/leads/${email}`,
+          data: {
+            dateClient,
           }
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        })
+        });
+      }
     })
     .catch(function (error) {
       // handle error
       console.log(error);
     })
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
 
   return null
 }
